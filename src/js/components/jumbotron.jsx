df3e8c24dbd05b194a1cd0432a503fac59bf0eba
Pedrocoll-creator <collpayeraspedro@gmail.com>
2025-03-15 11:58:14 +0000

    First Commit

--- src/js/components/jumbotron.jsx
+++ src/js/components/jumbotron.jsx
@@ -6,7 +6,7 @@ const jumbotron = () => {
     return (
         <div className="mb-4 bg-light rounded-3 mx-5 mt-4">
             <div className="container-fluid py-5 text-start">
-                <h1 className="display-5 fw-bold">3X!</h1>
+                <h1 className="display-5 fw-bold">3XXgit!</h1>
                 <p className="col-md-8 fs-4">
                     bienvenido a este prototipo de cinema online , disfruten de estas peliculas para los fanaticos de marvel
                 </p>
